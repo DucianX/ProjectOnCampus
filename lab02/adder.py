1c3643cd9fe596d51d3680297bd24cc63b8d38cc
DucianX <ducianx@gmail.com>
2024-02-12 11:55:45 -0800

    Leave big one

--- lab02/adder.py
+++
@@ -1,7 +0,0 @@
-def main():
-    fir = input("Enter a 1st value: ")
-    sec = input("Enter a 2nd value: ")
-    Sum = int(fir) + int(sec)
-    print("The sum of " + str(fir) + ' + ' + str(sec) + ' is ' + str(Sum))
-
-main()
